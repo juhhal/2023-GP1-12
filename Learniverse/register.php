201fb5435a00499fe2f418e6e4300267b01b4d6c
Shouq Alotaibi <112511693+shouqcodes@users.noreply.github.com>
2024-03-14 04:57:09 +0300

    fixed register errors

--- Learniverse/register.php
+++ Learniverse/register.php
@@ -17,7 +17,7 @@ $firstname = "";
 $lastname = "";
 $email = "";
 $password = "";
-$response = "";
+$response = [];
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -26,41 +26,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lastname = htmlspecialchars($_POST["lastname"]);
     $email = strtolower($_POST["email"]);
     $password = $_POST["password"];
-    if (strlen($_POST["password"]) < 12) {
-        $response = "Password must be at least 12 character.";
-    } else if (!preg_match('/^[A-Za-z]/', $firstname) || !preg_match('/^[A-Za-z]/', $lastname)) {
-        $response = "The first letter of both the first name and last name must be alphabetical characters.";
-    } else {
-        $password = sha1(htmlspecialchars($_POST["password"]));
-        // Sanitize and validate email
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $response = 'Invalid email address.';
-        } else {
-            // Perform register validation
-            $findemail = $Usercollection->findOne(['email' => $email]);
-            $findusername = $Usercollection->findOne(['username' => $username]);
-            if ($findemail) {
-                $response = 'Your are already registred! Try to login';
-            } else if ($findusername) {
-                $response = 'That username is taken. Try another one.';
-            } else {
-                $newUser = ['google_user_id' => null, 'username' => $username, 'firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'password' => $password, 'files_count' => 0, 'file_names' => ""];
-                $result = $Usercollection->insertOne($newUser);
 
-                if ($result) {
-                    $_SESSION['email'] = $email;
-                    require 'initialize_tools.php';
-                    // Successful register
-                    header("Location: workspace.php");
-                    exit();
-                } else {
-                    $response = 'Registration failed. Please try again later.';
-                }
-            }
+    $password = sha1(htmlspecialchars($_POST["password"]));
+    // Sanitize and validate email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    // Perform register validation
+    $findemail = $Usercollection->findOne(['email' => $email]);
+    $findusername = $Usercollection->findOne(['username' => $username]);
+
+    if (strlen($_POST["password"]) < 12)
+        $response['passwordError'] = "Password must be at least 12 character.";
+    if (!preg_match('/^[A-Za-z]/', $firstname))
+        $response['fnameError'] = "The first letter the first name must be an alphabetical character.";
+    if (!preg_match('/^[A-Za-z]/', $lastname))
+        $response['lnameError'] = "The first letter of the last name must be an alphabetical character.";
+    if ($findemail)
+        $response['emailError'] = 'You are already registred! Try to login';
+    if ($findusername)
+        $response['usernameError'] = 'That username is taken. Try another one.';
+
+    if (empty($response)) {
+        $newUser = ['google_user_id' => null, 'username' => $username, 'firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'password' => $password, 'files_count' => 0, 'file_names' => ""];
+        $result = $Usercollection->insertOne($newUser);
+
+        if ($result) {
+            $_SESSION['email'] = $email;
+            require 'initialize_tools.php';
+            // Successful register
+            header("Location: workspace.php");
+            exit();
+        } else {
+            $response['generalError'] = 'Registration failed. Please try again later.';
         }
     }
 }
+
 
 ?>
 
@@ -93,12 +94,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 <br>
 
-                <div id="response" style="color:red;"><?php echo $response; ?></div>
+                <div class="response" id="generalError" style="color:red;"><?php echo (isset($response['generalError'])) ? $response['generalError'] : "";
+                                                                            ?></div>
 
                 <div class="form-row">
                     <div class="input-group">
                         <label for="username">Username</label>
                         <input type="text" id="username" name="username" placeholder="" class="form-control" value="" required>
+                        <div class="response" id="usernameError" style="color:red;"><?php echo (isset($response['usernameError'])) ? $response['usernameError'] : ""; ?></div>
                     </div>
                 </div>
 
@@ -106,6 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="input-group">
                         <label for="firstname">First Name</label>
                         <input type="text" id="firstname" name="firstname" placeholder="Start with a letter" class="form-control" value="" required>
+                        <div class="response" id="fnameError" style="color:red;"><?php echo (isset($response['fnameError'])) ? $response['fnameError'] : ""; ?></div>
                     </div>
                 </div>
 
@@ -113,6 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="input-group">
                         <label for="lastname">Last Name</label>
                         <input type="text" id="lastname" name="lastname" placeholder="Start with a letter" class="form-control" value="" required>
+                        <div class="response" id="lnameError" style="color:red;"><?php echo (isset($response['lnameError'])) ? $response['lnameError'] : ""; ?></div>
                     </div>
                 </div>
 
@@ -120,6 +125,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="input-group">
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" placeholder="example@email.com" class="form-control" value="" required>
+                        <div class="response" id="emailError" style="color:red;"><?php echo (isset($response['emailError'])) ? $response['emailError'] : ""; ?></div>
                     </div>
                 </div>
 
@@ -127,6 +133,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="input-group">
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" placeholder="12 character" class="form-control" value="" required>
+                        <div class="response" id="passwordError" style="color:red;"><?php echo (isset($response['passwordError'])) ? $response['passwordError'] : ""; ?></div>
                     </div>
                 </div>
 
@@ -146,22 +153,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <img src="images/signup.png" alt='Advertising picture'>
         <div class="centered">
             <p id="content"> Not a member yet?<br>
-                Dive in and become a part of our ever-growing universe</p>
+                Dive in and become a part of our ever-growing universe</p>
 
         </div>
     </div>
 
 </body>
-<script src = "jquery.js"></script>
+<script src="jquery.js"></script>
 <script>
+    window.onload = function() {
+        <?php $response = [] ?>
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var inputs = document.querySelectorAll('input');
+
+        inputs.forEach(function(input) {
+            input.addEventListener('input', function() {
+                var errorDiv = this.nextElementSibling;
+                errorDiv.innerHTML = ''; // Clear the error message
+            });
+        });
+    });
+
     function validateForm(event) {
         event.preventDefault(); // Prevent the form from submitting by default
         console.log(event)
         var username = document.getElementById('username');
 
-        var error = document.getElementById('response');
+        var error = document.getElementById('usernameError');
 
-        if (username.value.trim() == '' ) {
+        if (username.value.trim() == '') {
             error.textContent = 'Please enter a valid nonempty username.'; // Display the error message
             return false; // Cancel form submission
         } else {
@@ -169,7 +191,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         // If the validation passes, you can proceed with form submission
-        document.querySelector('form').dispatchEvent(new Event('submit'));
+        document.querySelector('form').submit();
     }
 </script>
 
