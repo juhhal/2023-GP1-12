68490e597f48019d074e71fd6be1dfec82ee4f89
Shouq Alotaibi <112511693+shouqcodes@users.noreply.github.com>
2024-03-23 09:45:02 +0300

    added creatring a meeting room for shared spaces

--- Learniverse/addSharedSpace.php
+++ Learniverse/addSharedSpace.php
@@ -10,6 +10,47 @@ if (isset($_POST['spaceName']) && $_POST['spaceName'] != "") {
     $spaceName = $_POST['spaceName'];
     $spaceID  = uniqid();
     $spaceColor = $_POST['color'];
+
+    //create meeting room for the shared space
+    $roomUrl;
+    $hostURL;
+    $api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmFwcGVhci5pbiIsImF1ZCI6Imh0dHBzOi8vYXBpLmFwcGVhci5pbi92MSIsImV4cCI6OTAwNzE5OTI1NDc0MDk5MSwiaWF0IjoxNzEwODc0OTI3LCJvcmdhbml6YXRpb25JZCI6MjE5NDk2LCJqdGkiOiJkMDdjNDkwZS00MjJlLTQwMzctYWViOS00ODM2NTc1ZDQxZTMifQ.QnhnMtHYeDa__GtBYDNBAwJ31_dJ0SMhFigPwUKCrTg";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.whereby.dev/v1/meetings');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt(
+        $ch,
+        CURLOPT_POSTFIELDS,
+        '{
+    "endDate": "2099-02-18T14:23:00+03:00",
+    "roomMode":"group",
+    "fields": ["hostRoomUrl"]}'
+    );
+
+    $headers = [
+        'Authorization: Bearer ' . $api_key,
+        'Content-Type: application/json'
+    ];
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $response = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+    if ($httpcode !== 201) {
+        $error = [
+            "message" => "error",
+            "error" => [
+                "code" => $httpcode
+            ]
+        ];
+        echo json_encode($error);
+    } else {
+        $dataURL = json_decode($response);
+        $roomUrl = $dataURL->{'roomUrl'};
+        $hostURL = $dataURL->{'hostRoomUrl'};
+    }
     // Create space object
     $space = [
         'spaceID' => $spaceID,
@@ -21,6 +62,8 @@ if (isset($_POST['spaceName']) && $_POST['spaceName'] != "") {
         'tasks' => [],
         'files' => [],
         'feed' => [],
+        'hostUrl' => $hostURL,
+        'roomUrl' => $roomUrl,
         'logUpdates' => []
     ];
     $bulkWrite->insert($space);
@@ -37,7 +80,7 @@ if (isset($_POST['spaceName']) && $_POST['spaceName'] != "") {
     $bulk = new MongoDB\Driver\BulkWrite;
     $bulk->insert($spaceCalendar);
     $result2 = $manager->executeBulkWrite("Learniverse.calendar", $bulk);
-    echo json_encode(['msg' => 'Creating..', 'createdSpace' => "$spaceID"]);    
+    echo json_encode(['msg' => 'Creating..', 'createdSpace' => "$spaceID"]);
     exit();
 } elseif (isset($_POST['spaceID']) && $_POST['spaceID'] != "") {
 
